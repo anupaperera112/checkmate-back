bee35896aadab250c0c287779cfdb6735f44e5a9
Anupa Perera <anupaperera112@gmail.com>
2024-08-13 22:33:48 +0530

    basic structure

--- src/main/java/com/example/to_do/list/controller/TaskController.java
+++ src/main/java/com/example/to_do/list/controller/TaskController.java
@@ -1,4 +1,15 @@
 package com.example.to_do.list.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/task")
 public class TaskController {
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("hello");
+    }
 }
